de08846d62d48782896a2c65db08d0fa9d9528c5
feng <fefeng0504@163.com>
2018-02-27 16:43:01 +0800

    显示用户信息，登出用户

--- src/models/users.js
+++ src/models/users.js
@@ -3,18 +3,18 @@ import ajax from './ajax';
 const users = {
     getUser: () => { return ajax.get('/v1/users') },
     login: (body) => { return ajax.post('/v1/users/login', body) },
+    logout: () => { return ajax.delete(`/v1/users/logout`) },
     setDefaultToken: () => {
         if (!ajax.defaults.headers.common['token']) {
             let token = sessionStorage.getItem('token');
-            document.cookie = `token:${token}`;
-            sessionStorage.setItem('token', token);
             ajax.defaults.headers.common['token'] = token
         }
     },
-    setToken: (token) => {
-        document.cookie = `token:${token}`;
-        sessionStorage.setItem('token', token);
-        ajax.defaults.headers.common['token'] = token
+    setToken: (userInfo) => {
+        document.cookie = `token:${userInfo.token}`;
+        sessionStorage.setItem('token', userInfo.token);
+        sessionStorage.setItem('userInfo', JSON.stringify(userInfo));
+        ajax.defaults.headers.common['token'] = userInfo.token
     }
 };
 users.setDefaultToken();
